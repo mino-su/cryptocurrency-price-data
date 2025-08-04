8300a503ca691150bf4246680c71a739406643b9
mino-su <oms1218ju@gmail.com>
2025-08-04 12:23:33 +0900

    08.04 commit edit applications, build main.yaml

--- src/main/java/cryptocurrency/price/PriceApplication.java
+++ src/main/java/cryptocurrency/price/PriceApplication.java
@@ -3,13 +3,15 @@ package cryptocurrency.price;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication()
 public class PriceApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(PriceApplication.class, args);
+		ConfigurableApplicationContext run = SpringApplication.run(PriceApplication.class, args);
+		SpringApplication.exit(run);
 	}
 
 }
